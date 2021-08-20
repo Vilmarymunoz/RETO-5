2761d8c08bf922115d7f10f135adab392976d326
Vilmarymunoz <86936836+Vilmarymunoz@users.noreply.github.com>
2021-08-20 05:04:23 -0500

    Add files via upload

---
+++ reto4_p66_esqueleto/src/main/java/utp/misiontic2022/c2/p66/reto4/vista/ControladorRequerimientos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utp.misiontic2022.c2.p66.reto4.vista;
+
+import java.util.ArrayList;
+import utp.misiontic2022.c2.p66.reto4.modelo.vo.Requerimiento_3;
+
+/**
+ *
+ * @author vilma
+ */
+class ControladorRequerimientos {
+
+    ArrayList<Requerimiento_3> consultarRequerimiento3() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
